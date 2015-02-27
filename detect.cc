6fb01dd675e0e271ea644163e527958dc3e115fa
Michael Sergio <mikeserg@gmail.com>
2015-02-26 20:39:36 -0500

    Works... almost

--- detect.cc
+++ detect.cc
@@ -6,15 +6,15 @@
 #define CARD "card_table.png"
 
 
-void img_display(cv::Mat image) {
+void display(cv::Mat image) {
   cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
   imshow("Display Image", image);
-  img_display(image);
+  cv::waitKey(0);
 }
 
 int main(int argc, const char *argv[])
 {
-  std::cout << "Hello World!\n";
+  //std::cout << "Hello World!\n";
   cv::Mat image, dst, cdst;
   image = cv::imread(CARD, 1 );
 
@@ -26,17 +26,18 @@ int main(int argc, const char *argv[])
   cv::Canny(image, cdst, 50, 200, 3);
   cv::cvtColor(cdst, dst, cv::COLOR_GRAY2BGR);
 
+  // display(cdst);
+
   // Hough Line finder.
   // gets vector of lines (p, theta) (line-normal form)
   std::vector<cv::Vec4i> lines;
   double rho = 1;
   double theta = CV_PI/180;
   int threshold = 50;
-  cv::HoughLines(image, lines, rho, theta, threshold);
+  cv::HoughLinesP(cdst, lines, rho, theta, threshold);
 
   // Draw lines on image
-  for(size_t i = 0; i < lines.size(); i++)
-  {
+  for (size_t i = 0; i < lines.size(); i++) {
     cv::Vec4i l = lines[i];
     int line_aa = 16;
     cv::line(dst,
@@ -46,7 +47,7 @@ int main(int argc, const char *argv[])
              3, CV_AA);
   }
 
+  display(dst);
 
-  cv::waitKey(0);
   return 0;
 }
